04832dae46587b11fe99890eba0bf84ffba3e38a
Your Name <you@example.com>
2019-09-02 14:35:39 +0100

    updated the php file

--- data_type.php
+++ data_type.php
@@ -1,24 +1,17 @@
 <?php 
-    $a = 4500;
-    $b = 4500;
-    $acc_salary_a = $a + 1200;
-    $acc_salary_b = $b + 1500.;
+    $base_salary = 4500;
+    $acc_salary_a = $base_salary + 1200;
+    $acc_salary_b = $base_salary + 1500.;
     echo $acc_salary_a . " is the accumulated salary for A";
-echo "<br>";  
-    echo $acc_salary_b . " is the accumulated salary for B";
-echo "<br>";
 
+    echo $acc_salary_b . " is the accumulated salary for B";
     $earn_a = $acc_salary_a / 30;
     $earn_b = $acc_salary_b / 28;
     echo $earn_a . " is the amount earned by A in a day";
-echo "<br>"; 
     echo $earn_b . " is the amount earned by B in a day";
-echo "<br>"; 
     echo "Yes, i can";
-echo "<br>";
     $money_earn_a = $earn_a * 5;
     $money_earn_b = $earn_b * 5;
     echo $money_earn_a . " is the amount earned by A in 5 days";
-echo "<br>"; 
     echo $money_earn_b . " is the amount earned by B in 5 days";
 ?>
