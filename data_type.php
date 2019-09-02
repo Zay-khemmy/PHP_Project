c3cfb6716623709bb557e4b0c499c31522fb4f1b
Your Name <you@example.com>
2019-09-02 14:11:12 +0100

    my php assessment

---
+++ data_type.php
@@ -0,0 +1,24 @@
+<?php 
+    $a = 4500;
+    $b = 4500;
+    $acc_salary_a = $a + 1200;
+    $acc_salary_b = $b + 1500.;
+    echo $acc_salary_a . " is the accumulated salary for A";
+echo "<br>";  
+    echo $acc_salary_b . " is the accumulated salary for B";
+echo "<br>";
+
+    $earn_a = $acc_salary_a / 30;
+    $earn_b = $acc_salary_b / 28;
+    echo $earn_a . " is the amount earned by A in a day";
+echo "<br>"; 
+    echo $earn_b . " is the amount earned by B in a day";
+echo "<br>"; 
+    echo "Yes, i can";
+echo "<br>";
+    $money_earn_a = $earn_a * 5;
+    $money_earn_b = $earn_b * 5;
+    echo $money_earn_a . " is the amount earned by A in 5 days";
+echo "<br>"; 
+    echo $money_earn_b . " is the amount earned by B in 5 days";
+?>
