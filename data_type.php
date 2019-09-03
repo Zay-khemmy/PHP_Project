fc46bf254d624a4fa17fbdf406971f944b573c06
Your Name <you@example.com>
2019-09-03 09:15:52 +0100

    added comments to my code

--- data_type.php
+++ data_type.php
@@ -1,17 +1,23 @@
 <?php 
+// calculating accumulated salaries for A and B
     define('base_salary' , 4500);
     $acc_salary_a = base_salary + 1200;
     $acc_salary_b = base_salary + 1500.;
     echo $acc_salary_a . " is the accumulated salary for A";
 
     echo $acc_salary_b . " is the accumulated salary for B";
+
+    // calculating the amount earned by A and B each day
     $earn_a = $acc_salary_a / 30;
     $earn_b = $acc_salary_b / 28;
     echo $earn_a . " is the amount earned by A in a day";
     echo $earn_b . " is the amount earned by B in a day";
     echo "Yes, i can";
-    $money_earn_a = $earn_a * 5;
-    $money_earn_b = $earn_b * 5;
+
+    // calculating amount earned by both A and B for 5days
+    $five = 5;
+    $money_earn_a = $earn_a * $five;
+    $money_earn_b = $earn_b * $five;
     echo $money_earn_a . " is the amount earned by A in 5 days";
     echo $money_earn_b . " is the amount earned by B in 5 days";
 ?>
