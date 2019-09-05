cb20f642171b63cc360e5e3b60ed017f856634f5
Your Name <you@example.com>
2019-09-05 10:36:39 +0100

    updated function file

--- function.php
+++ function.php
@@ -3,14 +3,13 @@
     define('rate', 3.5);
     function simple_interest($p, $t){
         $s_i = $p * rate * $t / 100;
-        echo $s_i;
+        return $s_i;
     }
-    simple_interest(100, 5);
  
     // calculating product of variable
     function product($a, $b){
         $z = $a * $b;
         return $z;
     }
-    echo product(10, 10);
+    
 ?>
