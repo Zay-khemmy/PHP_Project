c964da0ef8e16824e1cd25334b96f9911f27bc67
Your Name <you@example.com>
2019-09-10 12:54:32 +0100

    updated foreach file

--- foreach.php
+++ foreach.php
@@ -1,25 +1,20 @@
 <?php 
-    //sum of array
+    // sum of array
+    $sum = 0;
     $a = array(1,2,3,4,5,6);
     foreach($a as $value){
-        
+        $sum += $value;
     }
-    echo array_sum($a);
+    echo ($sum);
 
-    //sum of value
-    $b =array('tech' => 4, 'admin' => 5,'hr' => 3,'finance' => 4);
-    foreach ($b as $key=>$value){
-       }
-       
-    echo array_sum($b);
-
+    // sum of value
     $sum = 0;
     $b =array('tech' => 4, 'admin' => 5,'hr' => 3,'finance' => 4);
     foreach ($b as $key=>$value){
         $sum += $value;
        }
        
-    echo($sum);
+    echo ($sum);
 
     // total number of keys
     echo count($b);
