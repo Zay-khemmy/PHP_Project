897eec15e7a00ee0f6d117d33c4edd873cb3a2e0
Your Name <you@example.com>
2019-09-11 12:23:23 +0100

    addded calculator file

---
+++ calculator.php
@@ -0,0 +1,26 @@
+<?php 
+   // calculating with operations
+   function calculator($operation, $a, $b){
+        switch($operation){
+            case "add":
+            $result = $a + $b;
+            break;
+
+            case "subtract":
+            $result = $a - $b;
+            break;
+
+            case "multiply":
+            $result = $a * $b;
+            break;
+
+            case "divide":
+            $result = $a / $b;
+            break;
+            
+            default:
+            echo "invalid action";
+        }
+        echo $result;
+    }
+?>
